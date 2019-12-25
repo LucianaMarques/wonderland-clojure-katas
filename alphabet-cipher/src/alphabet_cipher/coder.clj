bf3ad67ebec13cc113e20f9a4895f9c2d99c9fe3
Luciana Marques <luciana.marques@luciana.marques>
2019-12-25 11:43:54 -0300

    Fixed memory leak in encode function

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -29,25 +29,23 @@
                  :z "zabcdefghijklmnopqrstuvwxy"})
 
 (defn get_letter [column row]
-  (char (rem (+ (int row)
-                (- (int column)
-                   (int \a)))
-             (int \z))))
+  (def temp_res (+ (int column) (- (int row) (int \a))))
+  (if (<= temp_res (int \z))
+    (char temp_res)
+    (char (+ (int (rem temp_res (int \z))) (- (int \a) 1)))))
 
-(defn concat_keyword [keyword times]
+(defn concat_keyword_recursive [keyword times]
   (if (= times 1)
     (str keyword)
-    (concat_keyword (str keyword keyword) (- times 1))))
+    (concat_keyword_recursive (str keyword keyword) (- times 1))))
 
 (defn create_repeated_keyword [keyword size]
-  (def keyword_seq (seq keyword))
-  (take size (concat_keyword keyword size)))
+  (def max_size (int (+ (rem (count keyword) size) (/ (count keyword) size))))
+  (take size (concat_keyword_recursive keyword max_size)))
 
 (defn encode [keyword message]
   (def keyword_seq (create_repeated_keyword keyword (count message)))
-  (map get_letter keyword_seq message))
-
-(encode "vigilance" "meetmeontuesdayeveningatseven")
+  (apply str (map get_letter keyword_seq message)))
 
 (defn decode [keyword message]
   "decodeme")
