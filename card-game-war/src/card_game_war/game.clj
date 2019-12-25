e2ef6797be29deef663150e2c0e88fe5bb38c70a
Luciana Marques <luciana.marques@luciana.marques>
2019-12-25 18:45:04 -0300

    Implement play-round function

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,25 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn- play-tie-round [card1 card2]
+  (println card1)
+  (println card2))
 
-(defn play-game [player1-cards player2-cards])
+(defn- get-rank [card]
+  (.indexOf ranks (peek card)))
+
+(defn- get-suit [card]
+  (.indexOf suits (first card)))
+
+(defn play-round [player1-card player2-card]
+  (def suit1 (get-suit player1-card))
+  (def suit2 (get-suit player2-card))
+  (def rank1 (get-rank player1-card))
+  (def rank2 (get-rank player2-card))
+  (if (and (= suit1 suit2) (= rank1 rank2))
+    (play-tie-round player1-card player2-card))
+  (if (> rank1 rank2) rank1 rank2)
+  (if (> suit1 suit2) suit1 suit2))
+
+(defn play-game [player1-cards player2-cards]
+  (map play-round player1-cards player2-cards))
