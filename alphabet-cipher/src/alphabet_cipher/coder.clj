b302fc26999e8f080596ba40ca56aff482d1c069
Luciana Marques <luciana.marques@luciana.marques>
2019-12-25 00:05:02 -0300

    Dummy encode version

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,64 +1,53 @@
 (ns alphabet-cipher.coder)
 (require '[clojure.string :as str])
 
-(def letter_map {:A "abcdefghijklmnopqrstuvwxyz",
-                 :B "bcdefghijklmnopqrstuvwxyza",
-                 :C "cdefghijklmnopqrstuvwxyzab",
-                 :D "defghijklmnopqrstuvwxyzabc",
-                 :E "efghijklmnopqrstuvwxyzabcd",
-                 :F "fghijklmnopqrstuvwxyzabcde",
-                 :G "ghijklmnopqrstuvwxyzabcdef",
-                 :H "hijklmnopqrstuvwxyzabcdefg",
-                 :I "ijklmnopqrstuvwxyzabcdefgh",
-                 :J "jklmnopqrstuvwxyzabcdefghi",
-                 :K "klmnopqrstuvwxyzabcdefghij",
-                 :L "lmnopqrstuvwxyzabcdefghijk",
-                 :M "mnopqrstuvwxyzabcdefghijkl",
-                 :N "nopqrstuvwxyzabcdefghijklm",
-                 :O "opqrstuvwxyzabcdefghijklmn",
-                 :P "pqrstuvwxyzabcdefghijklmno",
-                 :Q "qrstuvwxyzabcdefghijklmnop",
-                 :R "rstuvwxyzabcdefghijklmnopq",
-                 :S "stuvwxyzabcdefghijklmnopqr",
-                 :T "tuvwxyzabcdefghijklmnopqrs",
-                 :U "uvwxyzabcdefghijklmnopqrst",
-                 :V "vwxyzabcdefghijklmnopqrstu",
-                 :W "wxyzabcdefghijklmnopqrstuv",
-                 :X "xyzabcdefghijklmnopqrstuvw",
-                 :Y "yzabcdefghijklmnopqrstuvwx",
-                 :Z "zabcdefghijklmnopqrstuvwxy"})
+(def letter_map {:a "abcdefghijklmnopqrstuvwxyz"
+                 :b "bcdefghijklmnopqrstuvwxyza"
+                 :c "cdefghijklmnopqrstuvwxyzab"
+                 :d "defghijklmnopqrstuvwxyzabc"
+                 :e "efghijklmnopqrstuvwxyzabcd"
+                 :f "fghijklmnopqrstuvwxyzabcde"
+                 :g "ghijklmnopqrstuvwxyzabcdef"
+                 :h "hijklmnopqrstuvwxyzabcdefg"
+                 :i "ijklmnopqrstuvwxyzabcdefgh"
+                 :j "jklmnopqrstuvwxyzabcdefghi"
+                 :k "klmnopqrstuvwxyzabcdefghij"
+                 :l "lmnopqrstuvwxyzabcdefghijk"
+                 :m "mnopqrstuvwxyzabcdefghijkl"
+                 :n "nopqrstuvwxyzabcdefghijklm"
+                 :o "opqrstuvwxyzabcdefghijklmn"
+                 :p "pqrstuvwxyzabcdefghijklmno"
+                 :q "qrstuvwxyzabcdefghijklmnop"
+                 :r "rstuvwxyzabcdefghijklmnopq"
+                 :s "stuvwxyzabcdefghijklmnopqr"
+                 :t "tuvwxyzabcdefghijklmnopqrs"
+                 :u "uvwxyzabcdefghijklmnopqrst"
+                 :v "vwxyzabcdefghijklmnopqrstu"
+                 :w "wxyzabcdefghijklmnopqrstuv"
+                 :x "xyzabcdefghijklmnopqrstuvw"
+                 :y "yzabcdefghijklmnopqrstuvwx"
+                 :z "zabcdefghijklmnopqrstuvwxy"})
 
-(defn create_letter_map []
-  {})
+(defn get_letter [column row]
+  (char (rem (+ (int row)
+                (- (int column)
+                   (int \a)))
+             (int \z))))
 
 (defn concat_keyword [keyword times]
   (if (= times 1)
     (str keyword)
     (concat_keyword (str keyword keyword) (- times 1))))
 
-(concat_keyword "stone" 2)
-
 (defn create_repeated_keyword [keyword size]
   (def keyword_seq (seq keyword))
   (take size (concat_keyword keyword size)))
 
-;; (take size (for [char keyword_seq] (int char)))))
-
-(create_repeated_keyword "abc" 1)
-
-(create_repeated_keyword "abc" 5)
-
-
-;; create repeated keyword string
-;; create string map
-;; access string map by keyword and message string
-;; return it
-;; (def m (str/split message #""))
-
 (defn encode [keyword message]
-  (int (first (seq message))))
+  (def keyword_seq (create_repeated_keyword keyword (count message)))
+  (map get_letter keyword_seq message))
 
-(encode "key" "ma")
+(encode "vigilance" "meetmeontuesdayeveningatseven")
 
 (defn decode [keyword message]
   "decodeme")
