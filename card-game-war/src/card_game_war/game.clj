b8bac58415626182280129b812ace55c719100df
Luciana Marques <luciana.marques@luciana.marques>
2019-12-26 22:41:14 -0300

    Save current stage

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -27,27 +27,24 @@
 (defn- update-player-cards [player-cards cards-to-add]
   (concat (rest player-cards) [(first player-cards)] cards-to-add))
 
-(defn- play-turn [player1-cards player2-cards turn]
+(defn- play-turn [player1-cards player2-cards turn cards-to-add]
   (println "Turn " turn)
+  (if (empty? (rest player1-cards)) "Player 2 wins"
   (println "Player 1 cards " player1-cards)
   (println "Player 2 cards " player2-cards)
   (def player1-card (first player1-cards))
   (def player2-card (first player2-cards))
-  (def winning-card (play-round player1-card player2-card))
-  (println "Winning card " winning-card)
+  (def round-result (play-round player1-card player2-card))
+  (println "Round result: " winning-card)
   (cond
-    (= winning-card nil) )
-  (if (= winning-card player1-card)
-    (if (empty? (rest player2-cards)) "Player 1 wins"
-                                      (play-turn (update-player-cards player1-cards [player2-card])
-                                                 (rest player2-cards)
-                                                 (+ turn 1)))
-    (if (empty? (rest player1-cards)) "Player 2 wins"
-                                      (play-turn (rest player1-cards)
-                                                 (update-player-cards player2-cards [player1-card])
-                                                 (+ turn 1)))))
+    (= round-result nil) (play-turn (rest player1-cards) (rest player2-cards) (+ turn 1)
+                                    (concat (first player1-cards) (first player2-cards))))
+    (= winning-card player1-card) (play-turn (update-player-cards player1-cards [player2-card])
+                                             (rest player2-cards) (+ turn 1) cards-to-add)
+    :else (play-turn (rest player1-cards) (update-player-cards player2-cards [player1-card])
+                     (+ turn 1) cards-to-add)))
 
-(defn play-game [player1-cards player2-cards turn]
+(defn play-game [player1-cards player2-cards]
   (cond
     (empty? player1-cards) "Player 2 wins"
     (empty? player2-cards) "Player 1 wins"
