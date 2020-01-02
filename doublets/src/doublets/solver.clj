fe8d7714e6450b62e7694c2d1a247cfc3ea60d63
Luciana Marques <luciana.marques@luciana.marques>
2020-01-02 14:09:01 -0300

    Add get-doublet function

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,16 +1,23 @@
 (ns doublets.solver
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
 
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
 
+(defn- get-doublet [word]
+  (filter (fn [w] (and (str/ends-with? w (apply str (rest word)))
+                       (not= w word))) words))
+
 (defn- get-doublets [word]
   )
 
 (defn doublets [word1 word2]
-  (if (= (count word1) (count word2))
-    (doublets (get-doublet word1) word2)
-    []))
+  (cond
+    (= (count word1) (count word2)) (get-doublet word1)
+    :else []))
+
+(doublets "look" "lock")
