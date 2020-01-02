bc16f27c158ac0f68857a468b58daea3cd4f372f
Luciana Marques <luciana.marques@luciana.marques>
2020-01-02 18:56:44 -0300

    Update get-doublets function

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -11,21 +11,20 @@
 (defn- get-distance [word1 word2]
   (->> (map = word1 word2) (remove true?) count))
 
-(defn- get-doublets [word]
-  (filter (fn [w] (and (= (count word) (count w))
-                       (= 1 (get-distance word w)))) words))
-
 (defn- sort-by-distance [target-word words]
   (sort-by #(get-distance target-word %) words))
 
 (defn- remove-already-visited [visited-words doublets]
   (remove #(some #{%} visited-words) doublets))
 
+(defn- get-doublets [word visited]
+  (->> (filter (fn [w] (and (= (count word) (count w))
+                            (= 1 (get-distance word w)))) words)
+       (remove-already-visited visited) (sort-by-distance word)))
+
 (defn doublets [word1 word2]
   (cond
     (= (count word1) (count word2)) (if (some #{word2} (get-doublets word1)) word2 [])
     :else []))
 
-(sort-by-distance "lock" (get-doublets "look"))
-(get-doublets "look")
 (doublets "look" "lock")
