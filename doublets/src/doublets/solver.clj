cc6acdc5a1feb7d1276425769779587214ef49a8
Luciana Marques <luciana.marques@luciana.marques>
2020-01-02 18:24:51 -0300

    Add sort-by-distance function

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -12,12 +12,16 @@
   (->> (map = word1 word2) (remove true?) count))
 
 (defn- get-doublets [word]
-  (filter (fn [w] (and (= (count word) (count w)) (= 1(get-distance word w)))) words))
+  (filter (fn [w] (and (= (count word) (count w)) (= 1 (get-distance word w)))) words))
+
+(defn- sort-by-distance [word1 words]
+  (sort-by #(get-distance word1 %) words))
 
 (defn doublets [word1 word2]
   (cond
     (= (count word1) (count word2)) (if (some #{word2} (get-doublets word1)) word2 [])
     :else []))
 
+(sort-by-distance "lock" (get-doublets "look"))
 (get-doublets "look")
 (doublets "look" "lock")
