b593a655c723dabb9c59d4ba0ac41109f82b9948
Luciana Marques <luciana.marques@luciana.marques>
2020-01-02 16:44:52 -0300

    Add check for word2 in word1 direct doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -14,11 +14,10 @@
 (defn- get-doublets [word]
   (filter (fn [w] (and (= (count word) (count w)) (differs-by-one-letter word w))) words))
 
-(get-doublets "book")
-
 (defn doublets [word1 word2]
   (cond
-    (= (count word1) (count word2)) (get-doublets word1)
+    (= (count word1) (count word2)) (if (some #{word2} (get-doublets word1)) word2 [])
     :else []))
 
+(get-doublets "book")
 (doublets "look" "lock")
