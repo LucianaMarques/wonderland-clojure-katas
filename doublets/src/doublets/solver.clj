c68852bbaf525a116de97fc19a2c492fa29ca96c
Luciana Marques <luciana.marques@luciana.marques>
2020-01-02 18:41:53 -0300

    Add remove-already-visited function

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -12,10 +12,14 @@
   (->> (map = word1 word2) (remove true?) count))
 
 (defn- get-doublets [word]
-  (filter (fn [w] (and (= (count word) (count w)) (= 1 (get-distance word w)))) words))
+  (filter (fn [w] (and (= (count word) (count w))
+                       (= 1 (get-distance word w)))) words))
 
-(defn- sort-by-distance [word1 words]
-  (sort-by #(get-distance word1 %) words))
+(defn- sort-by-distance [target-word words]
+  (sort-by #(get-distance target-word %) words))
+
+(defn- remove-already-visited [visited-words doublets]
+  (remove #(some #{%} visited-words) doublets))
 
 (defn doublets [word1 word2]
   (cond
